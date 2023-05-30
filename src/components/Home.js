b53097180f9cffd3f356c4d121394ad6717486e5
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-05-30 17:34:29 +0530

    Section Component Added

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import Section from './Section';
+
+const Home = () => {
+  return (
+    <Container>
+      <Section />
+      <Section />
+      <Section />
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
